509b10939c995b747be25542ccc4cf32ce4a30da
Anton Tsoy <tcoy210@gmail.com>
2025-09-18 16:13:29 +0700

    Add: CRUD operations with database

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -22,4 +22,19 @@ func main() {
 	if err = db.HealthCheck(); err != nil {
 		log.Fatalf("не удалось открыть соединение c базой данных: %v", err)
 	}
+
+	//subsRepo := repository.NewSubsRepo(db.DB())
+
+	/*
+		userId, _ := uuid.Parse("60601fee-2bf1-4721-ae6f-7636e79a0cba")
+		layout := "01-2006"
+		startDate, _ := time.Parse(layout, "07-2025")
+		testModel := models.Subscription{ServiceName: "Spotify", Price: 20, UserID: userId, StartDate: startDate}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		defer cancel()
+		subsRepo.Create(ctx, &testModel)
+
+		fmt.Printf("%v", testModel)
+	*/
 }
--- internal/repository/subscription.go
+++ internal/repository/subscription.go
@@ -19,28 +19,78 @@ func NewSubsRepo(db *sqlx.DB) *SubsRepo {
 func (r *SubsRepo) Create(ctx context.Context, sub *models.Subscription) error {
 	query := `
         INSERT INTO subscriptions (service_name, price, user_id, start_date, end_date)
-        VALUES ($1, $2, $3, $4, $5)
+        VALUES (:service_name, :price, :user_id, :start_date, :end_date)
         RETURNING id
     `
 
-	var endDate any = nil
-	if sub.EndDate != nil {
-		endDate = *sub.EndDate
-	}
-
-	err := r.db.QueryRowContext(
-		ctx,
-		query,
-		sub.ServiceName,
-		sub.Price,
-		sub.UserID,
-		sub.StartDate,
-		endDate,
-	).Scan(&sub.ID)
-
+	stmt, err := r.db.PrepareNamed(query)
 	if err != nil {
-		return fmt.Errorf("failed to create subscription: %w", err)
+		return fmt.Errorf("подготовка записи подписки: %w", err)
 	}
 
+	return stmt.QueryRowxContext(ctx, sub).Scan(&sub.ID)
+}
+
+func (r *SubsRepo) GetByID(ctx context.Context, id int) (*models.Subscription, error) {
+	query := `SELECT * FROM subscriptions WHERE id=$1`
+
+	var sub models.Subscription
+	if err := r.db.GetContext(ctx, &sub, query, id); err != nil {
+		return nil, fmt.Errorf("ошибка получения подписки: %w", err)
+	}
+
+	return &sub, nil
+}
+
+func (r *SubsRepo) GetAll(ctx context.Context, limit, offset int) ([]models.Subscription, error) {
+	query := `
+        SELECT * FROM subscriptions
+        ORDER BY id
+        LIMIT $1 OFFSET $2
+    `
+
+	var subs []models.Subscription
+	if err := r.db.SelectContext(ctx, &subs, query, limit, offset); err != nil {
+		return nil, fmt.Errorf("ошибка получения подписок: %w", err)
+	}
+
+	return subs, nil
+}
+
+func (r *SubsRepo) Update(ctx context.Context, sub *models.Subscription) error {
+	query := `
+        UPDATE subscriptions
+        SET service_name = :service_name,
+            price = :price,
+            user_id = :user_id,
+            start_date = :start_date,
+            end_date = :end_date
+        WHERE id = :id
+    `
+
+	res, err := r.db.NamedExecContext(ctx, query, sub)
+	if err != nil {
+		return fmt.Errorf("ошибка обновления записи: %w", err)
+	}
+
+	if rows, err := res.RowsAffected(); err != nil || rows == 0 {
+		return fmt.Errorf("не удалось получить обновленную запись c id=%d", sub.ID)
+	}
 	return nil
 }
+
+func (r *SubsRepo) Delete(ctx context.Context, id int) error {
+	query := `DELETE FROM subscriptions WHERE id=$1`
+
+	res, err := r.db.ExecContext(ctx, query, id)
+	if err != nil {
+		return fmt.Errorf("ошибка удаления записи: %w", err)
+	}
+
+	if rows, err := res.RowsAffected(); err != nil || rows == 0 {
+		return fmt.Errorf("не удалось удалить запись c id=%d", id)
+	}
+	return nil
+}
+
+// func (r *SubsRepo) Aggr(ctx context.Context, userID uuid.UUID, serviceName string) ([]models.Subscription, error) {}
